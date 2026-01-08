5a0b4d02834f07865e18dca31b803cd967564251
Scott Weaver <scottmweaver@gmail.com>
2026-01-08 14:05:03 -0800

    Set up base for SVN deploy
    - Updated .distignore to include additional files and directories for exclusion.
    - Modified .gitignore to streamline ignored files.
    - Added .nvmrc for Node version management.
    - Introduced .wp-env.json for WordPress environment configuration.
    - Updated package.json with new scripts and dependencies.
    - Removed obsolete asset files from the build directory.

--- assets/build/admin.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '24ec71bbafad02aa056f');
--- assets/build/devmode.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'c2331a18e3a8ead9bf3e');
